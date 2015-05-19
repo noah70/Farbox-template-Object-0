223b6697a26e12468dd44719f4d79e8dc59ee2c2
Noah70 <noah70@users.noreply.github.com>
2015-05-19 14:02:06 +0800

    A simple Farbox template

---
+++ template/extra.js
@@ -0,0 +1,14 @@
+$(function(){
+    $("#gotop").click(function(){
+        jQuery("html,body").animate({
+            scrollTop:0
+        },1000);
+    });
+    $(window).scroll(function() {
+        if ( $(this).scrollTop() > 300){
+            $('#gotop').fadeIn("fast");
+        } else {
+            $('#gotop').stop().fadeOut("fast");
+        }
+    });
+});
